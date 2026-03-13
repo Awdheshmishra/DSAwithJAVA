24b92d33239119fc2e4d04519ce1c91e6ad65e06
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-03-13 23:39:52 +0530

    Update code

--- Recursion/Increasing.java
+++ Recursion/Increasing.java
@@ -1,14 +1,6 @@
 package Recursion;
-
 import java.util.Scanner;
-
 public class Increasing {
-    /* package whatever; // don't place package name! */
-
-
-
-    /* Name of the class has to be "Main" only if the class is public. */
-
         static int n ;
         public static void print(int x){
             if(x>n) return ;
@@ -19,9 +11,9 @@ public class Increasing {
         {
             Scanner sc = new Scanner(System.in);
             System.out.println("enter n");
-
-            int n = sc.nextInt();
+            n = sc.nextInt();
             print(1);
+            System.out.println();
 
         }
     }
