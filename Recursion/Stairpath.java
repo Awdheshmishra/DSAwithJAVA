20ac4ef5d2a8ccdd0ae1033e83238b1381a0ea9e
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-03-16 22:25:21 +0530

    fibonacci adding in recursion

---
+++ Recursion/Stairpath.java
@@ -0,0 +1,8 @@
+package Recursion;
+import java.util.Scanner;
+public class Stairpath {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter n:");
+    }
+}
