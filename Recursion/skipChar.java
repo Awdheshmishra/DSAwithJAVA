be8224334fa90adbcd5ffee50990bd49f241ee19
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-04-06 21:34:07 +0530

    adding skipchar.java

--- Recursion/skipChar.java
+++ Recursion/skipChar.java
@@ -1,4 +1,16 @@
 package Recursion;
 
 public class skipChar {
+    public static void skip (int i, String s, String ans){
+        if(i==s.length()){
+            System.out.println(ans);
+            return;
+        }
+        if(s.charAt(i)!='a')  ans+= s.charAt(i);
+        skip(i+1, s, ans);
+    }
+    public static void main(String[] args) {
+        String s = "Awadhesh Misahra";
+        skip(0, s, "");
+    }
 }
