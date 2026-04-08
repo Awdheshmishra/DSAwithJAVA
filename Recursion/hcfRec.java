2b342b9819dbc0df3cc634e1c4d3fbdf4769ef9a
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-04-08 23:56:40 +0530

    adding hcfrec

---
+++ Recursion/hcfRec.java
@@ -0,0 +1,18 @@
+package Recursion;
+import java.util.Scanner;
+public class hcfRec {
+    public static int hcf(int a, int b){
+        for(int i=Math.min(a,b);i>=1;i--){
+            if(a%i==0 && b%i==0) return i;
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a: ");
+        int a = sc.nextInt();
+        System.out.println("enter b: ");
+        int b = sc.nextInt();
+        System.out.println(hcf(a,b));
+    }
+}
