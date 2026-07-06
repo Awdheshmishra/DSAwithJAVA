12db4a46600d8c97cc9f7acf2bd3f15df3c50892
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-07-06 17:56:57 +0530

    added in stack

--- Stack/Insertion.java
+++ Stack/Insertion.java
@@ -1,4 +1,26 @@
 package Stack;
-
+import java.util.Stack;
 public class Insertion {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(3);
+        st.push(5);
+        st.push(0);
+        st.push(1);
+        st.push(7);
+        System.out.println(st);
+        int idx = 3; //index
+        int x = 12;
+        Stack<Integer> temp = new Stack<>();
+        while(st.size()>idx){
+            temp.push(st.pop());
+        }
+        st.push(x);
+        //push at bottom
+        while(temp.size()>0){
+            st.push(temp.pop());
+        }
+        System.out.println(st);
+
+    }
 }
---
+++ Stack/display.java
@@ -0,0 +1,50 @@
+package Stack;
+
+import java.util.Stack;
+
+public class display {
+    public static void displayReverse(Stack<Integer> st){
+//        s.push(6); //pass by refrence
+        if(st.size()==0) return;
+         int top = st.pop();
+        System.out.print(top+" ");
+        displayReverse(st);
+        st.push(top);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+//        System.out.println(st);
+        displayReverse(st);
+//        System.out.println(st);
+//        System.out.println(st);
+      /*  Stack<Integer> rt = new Stack<>();
+        while(st.size()>0){
+            rt.push(st.pop());
+        }
+
+
+        while (rt.size()>0){
+            int x = rt.pop();
+            System.out.print(x+" ");
+            st.push(x);
+        }
+        System.out.println(st);
+*/
+            //array
+       /* int n = st.size();
+        int [] arr = new int[n];
+        for (int i = n-1; i>=0;i--){
+            arr[i] = st.pop();
+        }
+        for (int i =0; i<n;i++){
+            System.out.print(arr[i]+ " ");
+            st.push(arr[i]);
+        }*/
+
+    }
+}
