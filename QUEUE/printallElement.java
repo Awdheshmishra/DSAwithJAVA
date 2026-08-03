dfd6260d7d704c923fe590efbe8c3df1fdcd24c1
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-08-03 17:00:47 +0530

    adding in queue

---
+++ QUEUE/printallElement.java
@@ -0,0 +1,27 @@
+package QUEUE;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+public class printallElement {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+//        System.out.print(q+" ");
+        Queue <Integer> helper = new ArrayDeque<>();
+        while (q.size()>0){
+            System.out.print(q.peek()+" ");
+            helper.add(q.poll());
+        }
+        while (helper.size()>0){
+            q.add(helper.poll());
+        }
+
+
+
+
+    }
+}
