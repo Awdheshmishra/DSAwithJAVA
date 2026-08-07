1fb83ab205486433ed55c4408f60b99c5d06b6cd
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-08-07 17:04:08 +0530

    adding in patterns

--- patterns/revision1.java
+++ patterns/revision1.java
@@ -34,6 +34,41 @@ public class revision1 {
                 System.out.println();
         }*/
 
+       /* for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i==j || i+j ==6) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }*/
+//binary triangle
+        /*for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i%2==1) {
+                    if (j % 2 == 1) {
+                        System.out.print(1);
+                    } else {
+                        System.out.print(0);
+                    }
+                }
+                else {//(i%2==0)
+                    if(j%2==0){
+                        System.out.print(1);
+                    }
+                    else {
+                        System.out.print(0);
+                    }
+                }
+            }
+            System.out.println();
+        }*/
+
+
+//        star filliped triangle
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i==j || i+j ==6) {
@@ -44,6 +79,7 @@ public class revision1 {
             }
             System.out.println();
         }
+
     }
 }
 
