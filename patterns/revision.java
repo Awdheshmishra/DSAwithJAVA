e2761a0e375a908c56fab42075e752dcd0bb1d68
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-08-10 19:19:40 +0530

    number pyramid palindrome

--- patterns/revision.java
+++ patterns/revision.java
@@ -107,12 +107,27 @@ public class revision {
 
         //star pyramid
 
-        for (int i = 1; i <= n; i++) {//rows
+       /* for (int i = 1; i <= n; i++) {//rows
             for (int j = 1; j <= n-i; j++) {//spaces
                 System.out.print(" "+ " ");
             }
             for(int j=1; j<=2*i-1;j++){//star
                 System.out.print("*" + " ");
+            }
+            System.out.println();
+        }*/
+
+        //Number pyramid palindrome
+
+        for (int i = 1; i <= n; i++) {//rows
+            for (int j = 1; j <= n-i; j++) {//spaces
+                System.out.print(" "+ " ");
+            }
+            for(int j=1; j<=i;j++){//star
+                System.out.print(j + " ");
+                }
+            for (int j=i-1;j>=1;j--){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
