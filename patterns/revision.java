6429326dd85121a78bb42bc868f52c3ae58d00ad
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-08-10 09:45:30 +0530

    adding in patterns

--- patterns/revision.java
+++ patterns/revision.java
@@ -1,6 +1,6 @@
 package patterns;
 import java.util.Scanner;
-public class revision1 {
+public class revision {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter n: ");
@@ -69,17 +69,29 @@ public class revision1 {
 
 //        star filliped triangle
 
-        for (int i = 1; i <= n; i++) {
+       /* for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (i==j || i+j ==6) {
+                if ( i+j >n) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
             }
             System.out.println();
-        }
+        }*/
 
+
+//        number triangle flipped --> composite pattern -- mixture of two pattern into one pattern
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" "+ " ");
+            }
+            for(int j=1; j<=i;j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
     }
 }
 
