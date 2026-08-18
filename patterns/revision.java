7e09cc3b2d31452230d1c38d5b6ea54d1a41abe9
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-08-18 07:31:46 +0530

    adding all types of patterns  in patterns

--- patterns/revision.java
+++ patterns/revision.java
@@ -5,15 +5,72 @@ public class revision {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter n: ");
         int n = sc.nextInt();
-       /* int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
+
+        // 0. STAR SQUARE
+
+        for (int i=1;i<=n;i++){
+            for (int j=1; j<=n;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+
+        //  1. NUMBER SQUARE
+
+       /* for (int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }*/
+
+
+        // 2.  ALPHABET SQUARE
+
+      /*  for (int i =1; i<=4; i++){
+            for (int j=1;j<=n;j++){
+                System.out.print((char)(j+64)+" "); //for capital letters
+//                System.out.print((char)(j+97)+" "); //for small letters
+            }
+            System.out.println();
+        }*/
+
+        //  3. NUMERIC AND ALPHABET TRIANGLE
+
+       /* for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                if(i%2==0){
+                    System.out.print((char)(j+64) + " ");
+                }
+                else {
+                    System.out.print(j+" ");
+                }
+            }
+                System.out.println();
+        }*/
+
+        //4 . REVERSE TRIANGLE
+
+       /* for (int i=1;i<=n;i++){
+            for (int j=1;j<=n+1-i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }*/
+
+
+        //  5. ODD TRIANGLE
+
+       /* for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(2*j-1+ " ");
             }
             System.out.println();
         }*/
 
-       /* int a = 1;
+        //  6. FLOYD TRIANGLE
+
+        /*int a = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(a+ " ");
@@ -22,8 +79,10 @@ public class revision {
             System.out.println();
         }*/
 
+         // 7. STAR PLUS
 
-       /* for (int i = 1; i <= n; i++) {
+
+        /*for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i==n/2+1 || j==n/2+1) {
                     System.out.print("*");
@@ -33,6 +92,8 @@ public class revision {
             }
                 System.out.println();
         }*/
+
+            // 8. STAR CROSS
 
        /* for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
@@ -44,7 +105,11 @@ public class revision {
             }
             System.out.println();
         }*/
-//binary triangle
+
+
+        //  9.  binary triangle
+
+
         /*for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 if (i%2==1) {
@@ -67,7 +132,7 @@ public class revision {
         }*/
 
 
-//        star filliped triangle
+        //    10.     star filliped triangle
 
        /* for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
@@ -81,7 +146,7 @@ public class revision {
         }*/
 
 
-//        number triangle flipped --> composite pattern -- mixture of two pattern into one pattern
+    //      11.   number triangle flipped --> composite pattern -- mixture of two pattern into one pattern
 
         /*for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n-i; j++) {
@@ -93,7 +158,7 @@ public class revision {
             System.out.println();
         }*/
 
-//        Rhombus
+        //       12.  Rhombus
 
        /* for (int i = 1; i <= n; i++) {//rows
             for (int j = 1; j <= n-i; j++) {//spaces
@@ -105,7 +170,7 @@ public class revision {
             System.out.println();
         }*/
 
-        //star pyramid
+        // 13.  star pyramid
 
        /* for (int i = 1; i <= n; i++) {//rows
             for (int j = 1; j <= n-i; j++) {//spaces
@@ -117,9 +182,9 @@ public class revision {
             System.out.println();
         }*/
 
-        //Number pyramid palindrome
+        //  14.  Number pyramid palindrome
 
-        for (int i = 1; i <= n; i++) {//rows
+      /*  for (int i = 1; i <= n; i++) {//rows
             for (int j = 1; j <= n-i; j++) {//spaces
                 System.out.print(" "+ " ");
             }
@@ -130,7 +195,93 @@ public class revision {
                 System.out.print(j+" ");
             }
             System.out.println();
+        }*/
+
+
+        // 15.  STAR BRIDGE
+
+       /* for (int i =1;i<=2*n-1;i++){
+            System.out.print("*"+" ");
         }
+        System.out.println();
+            n--;
+        for (int i=1; i<=n; i++) {//rows
+            for (int j = 1; j <= n + 1 - i; j++) {
+                System.out.print("*" + " ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(" " + " ");
+            }
+            for (int j = 1; j<=n+1-i; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }*/
+
+        // 16.  NUMBER BRIDGE
+
+        /*for (int i =1;i<=2*n-1;i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        n--;
+        int nsp = 1;
+        for (int i=1; i<=n; i++) {//rows
+            int a=1;
+            for (int j = 1; j <= n+1-i; j++) {
+                System.out.print(a++ +" ");
+            }
+            for (int j = 1; j <=nsp; j++) {
+                System.out.print(" " + " ");
+                a++;
+            }
+            nsp+=2;
+            for (int j = 5; j<=n+5-i; j++) {
+                System.out.print(a++ + " ");
+            }
+            System.out.println();
+        }*/
+
+        // 17.  STAR DIAMOND
+
+        /*int nsp = n-1;
+        int nst = 1;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=nsp;j++){
+                System.out.print(" "+" ");
+            }
+            for (int j =1;j<=nst;j++){
+                System.out.print("*"+" ");
+            }
+            nsp--;
+            nst+=2;
+            System.out.println();
+        }
+        nsp=1;
+        nst = nst-4;
+        for (int i=1;i<=n-1;i++){
+            for(int j=1;j<=nsp;j++){
+                System.out.print(" "+" ");
+            }
+            for (int j=1;j<=nst;j++){
+                System.out.print("*"+" ");
+            }
+            nsp++;
+            nst-=2;
+            System.out.println();
+        }*/
+
+        // 18. NUMBER SPIRAL
+
+       /* for (int i=1;i<2*n-1;i++){
+            for (int j=1;j<=2*n-1;j++){
+                int a = i, b=j;
+                if(i>n) a=2*n-i;
+                if(j>n) b=2*n-j;
+                System.out.print(n+1-Math.min(a,b)+" ");
+            }
+            System.out.println();
+        }*/
     }
 }
 
